0dab7c139a4344740c16598ee401a4126f2a72e2
ppiecha <piotr.piecha@outlook.com>
2025-05-11 15:27:19 +0200

    Next exercises - jupyter notebooks

--- amount-spent-sql.py
+++ amount-spent-sql.py
@@ -9,10 +9,12 @@ os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 spark = SparkSession.builder.appName("amount-spent-sql").getOrCreate()
 
-schema = StructType([StructField("client_id", IntegerType(), False),
-                     StructField("item_id", IntegerType(), False),
-                     StructField("amount", DoubleType(), False)
-])
+# schema = StructType([StructField("client_id", IntegerType(), False),
+#                      StructField("item_id", IntegerType(), False),
+#                      StructField("amount", DoubleType(), False)
+# ])
+
+schema = "client_id INT, item_id INT, amount DOUBLE"
 
 orders = spark.read.schema(schema=schema).csv("customer-orders.csv")
 orders.createOrReplaceTempView("orders")
@@ -23,7 +25,7 @@ spark.sql("""
            round(sum(amount), 2) as total
       from orders
      group by client_id
-     order by sum(amount) desc 
+     order by total desc 
 """).show(5)
 
 spark.stop()
